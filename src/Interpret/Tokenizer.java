fba959e31775dc4422635f824e83c401520744ae
Joshua Weinstein <jweinst1@berkeley.edu>
2015-12-25 14:55:46 -0800

    localdict implemented

--- src/Interpret/Tokenizer.java
+++ src/Interpret/Tokenizer.java
@@ -3,16 +3,30 @@ package Interpret;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import Interpret.Statement;
+import Interpret.localdict;
 
 public class Tokenizer {
     protected static Pattern plus = Pattern.compile("^(.+) plus (.+)$");
     protected static Pattern create = Pattern.compile("^create (.+) (.+)$");
     protected static Pattern connect = Pattern.compile("^connect (.+) to (.+)$");
+    protected static Pattern extend = Pattern.compile("^extend (.+) by (.+)$");
+    protected static Pattern assign = Pattern.compile("^assign (.+) to (.+)$");
 
-    //
+    //checks if create statement
     public static boolean iscreate(String test) {
         Matcher Cstate = create.matcher(test);
         return Cstate.matches();
+    }
+    //checks if assign statement
+    public static boolean isassign(String test) {
+        Matcher Cstate = assign.matcher(test);
+        return Cstate.matches();
+    }
+
+    public static boolean isIntContainer(String test) {
+        Pattern Icont = Pattern.compile("i\\[.*\\]i");
+        Matcher temp = Icont.matcher(test);
+        return temp.matches();
     }
 
     /*public static Statement tokenize(String input) {
--- src/Interpret/localdict.java
+++ src/Interpret/localdict.java
@@ -1,8 +1,32 @@
 package Interpret;
+import java.util.HashMap;
 /**
  * Created by Josh on 12/25/15.
  * Class that implements a hashmap for storing running variables and objects
  */
 public class localdict {
     //needs to store running variables
+    public HashMap<String, Object> bindings;
+    public boolean empty;
+
+    public localdict() {
+        bindings = new HashMap<String, Object>();
+        empty = true;
+    }
+    public int getsize() {
+        return bindings.size();
+    }
+    public void addbind(String name, Object value) {
+        bindings.put(name, value);
+    }
+    public Object getbind(String name) {
+        return bindings.get(name);
+    }
+    public void deletebind(String name) {
+        bindings.remove(name);
+    }
+    //checks if a binding exists
+    public boolean checkbind(String name) {
+        return bindings.containsKey(name);
+    }
 }
